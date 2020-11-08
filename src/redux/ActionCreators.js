70c80d3a3adf928b7a93610a212f15f1b2d4cf27
jonathanoleaz <jonathanoleaz@hotmail.com>
2020-11-08 09:37:47 -0600

    Fetch handling errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,8 +1,8 @@
 import * as ActionTypes from './ActionTypes';
 import { DISHES } from '../shared/dishes';
-import {baseUrl} from '../shared/baseUrl';
+import { baseUrl } from '../shared/baseUrl';
 
-export const addComment = (dishId, rating, author, comment) =>({
+export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
     payload: {
         dishId: dishId,
@@ -16,31 +16,46 @@ export const addComment = (dishId, rating, author, comment) =>({
 /**
  /**Returns an function that is going to dispatch or call several actions: dishesLoading and add dishes
  */
-export const fetchDishes = () =>(dispatch) => {
+export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
 
-    return fetch(baseUrl+'dishes')
-    .then(response => response.json())
-    .then(dishes => dispatch(addDishes(dishes)));
-    
+    return fetch(baseUrl + 'dishes')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            }
+            else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => {
+                var errmess = new Error(error.message);
+                throw errmess;
+            })
+        .then(response => response.json())
+        .then(dishes => dispatch(addDishes(dishes)))
+        .catch(error => dispatch(dishesFailed(error.message)));
+
     /*setTimeout(() => {
         dispatch(addDishes(DISHES));
     }, 2000);*/
 }
 
 /**Returns an action object */
-export const dishesLoading=()=>({
+export const dishesLoading = () => ({
     type: ActionTypes.DISHES_LOADING
 });
 
 /**Returns an action object */
-export const dishesFailed= (errmess)=>({
+export const dishesFailed = (errmess) => ({
     type: ActionTypes.DISHES_FAILED,
     payload: errmess
 });
 
 /**Returns an action object */
-export const addDishes = (dishes) =>({
+export const addDishes = (dishes) => ({
     type: ActionTypes.ADD_DISHES,
     payload: dishes
 });
@@ -50,12 +65,27 @@ export const addDishes = (dishes) =>({
 /**
  /**Returns an function that is going to dispatch or call several actions: dishesLoading and add dishes
  */
-export const fetchComments = () =>(dispatch) => {
+export const fetchComments = () => (dispatch) => {
     dispatch(dishesLoading(true));
 
-    return fetch(baseUrl+'comments')
-    .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)));
+    return fetch(baseUrl + 'comments')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            }
+            else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
     /*setTimeout(() => {
         dispatch(addDishes(DISHES));
     }, 2000);*/
@@ -63,13 +93,13 @@ export const fetchComments = () =>(dispatch) => {
 
 
 /**Returns an action object */
-export const commentsFailed= (errmess)=>({
+export const commentsFailed = (errmess) => ({
     type: ActionTypes.COMMENTS_FAILED,
     payload: errmess
 });
 
 /**Returns an action object */
-export const addComments = (comments) =>({
+export const addComments = (comments) => ({
     type: ActionTypes.ADD_COMMENTS,
     payload: comments
 });
@@ -83,30 +113,45 @@ export const addComments = (comments) =>({
 /**
  /**Returns an function that is going to dispatch or call several actions: dishesLoading and add dishes
  */
-export const fetchPromos = () =>(dispatch) => {
+export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading(true));
 
-    return fetch(baseUrl+'promotions')
-    .then(response => response.json())
-    .then(promos => dispatch(addPromos(promos)));
+    return fetch(baseUrl + 'promotions')
+    .then(response => {
+        if (response.ok) {
+            return response;
+        }
+        else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    })
+        .then(response => response.json())
+        .then(promos => dispatch(addPromos(promos)))
+        .catch(error => dispatch(promosFailed(error.message)));
     /*setTimeout(() => {
         dispatch(addDishes(DISHES));
     }, 2000);*/
 }
 
 /**Returns an action object */
-export const promosLoading=()=>({
+export const promosLoading = () => ({
     type: ActionTypes.PROMOS_LOADING
 });
 
 /**Returns an action object */
-export const promosFailed= (errmess)=>({
+export const promosFailed = (errmess) => ({
     type: ActionTypes.PROMOS_FAILED,
     payload: errmess
 });
 
 /**Returns an action object */
-export const addPromos = (promos) =>({
+export const addPromos = (promos) => ({
     type: ActionTypes.ADD_PROMOS,
     payload: promos
 });
